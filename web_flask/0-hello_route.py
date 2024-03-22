3ba9e85a294bd34d958c303027a5b21f0c426766
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-03-22 22:26:10 +0000

    feat: start a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""This module create and start a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """home: returns the string "Hello HBNB!",
+    when the user request the landing page
+
+    Returns:
+        str: the "Hello HBNB!" string to be rendered as HTML
+            element
+
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
